4030b37f6d6f93f50a04996dd6473fd6c394407c
microsoft <microsoft@microsoft-PC>
2018-11-26 14:05:48 +0545

    reter

--- src/com/collection/EvanStack.java
+++ src/com/collection/EvanStack.java
@@ -1,6 +1,22 @@
 package com.collection;
 
+import java.util.Iterator;
+import java.util.Stack;
+
 public class EvanStack
 	{
-
+		public static void main(String args[]){  
+			Stack<String> stack = new Stack<String>();  
+			stack.push("Ayush");  
+			stack.push("Garvit");  
+			stack.push("Amit");  
+			stack.push("Ashish");  
+			stack.push("Garima");  
+			stack.pop();  
+			stack.pop(); 
+			
+			System.out.println(stack.peek());
+			
+			
+	}
 	}
